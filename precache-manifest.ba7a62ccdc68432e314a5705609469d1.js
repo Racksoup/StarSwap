c56f8b10dd9fe9ef8f62ec6dfad6a5f45a7e973b
Racksoup <39200478+Racksoup@users.noreply.github.com>
2019-02-13 21:06:27 -0500

    Updates

---
+++ precache-manifest.ba7a62ccdc68432e314a5705609469d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c407022b05f1b6c1b47ba3993fae09c",
+    "url": "/StarSwap/static/media/unnamed.6c407022.png"
+  },
+  {
+    "revision": "4f41b80df90c846d41f5e4703c5e5120",
+    "url": "/StarSwap/static/media/starswap.4f41b80d.png"
+  },
+  {
+    "revision": "c7b3a72435c63c7bb655",
+    "url": "/StarSwap/static/js/runtime~main.c7b3a724.js"
+  },
+  {
+    "revision": "483f7d37295caa2b6faf",
+    "url": "/StarSwap/static/js/main.483f7d37.chunk.js"
+  },
+  {
+    "revision": "916b373cfef80c85a28b",
+    "url": "/StarSwap/static/js/1.916b373c.chunk.js"
+  },
+  {
+    "revision": "483f7d37295caa2b6faf",
+    "url": "/StarSwap/static/css/main.f671af30.chunk.css"
+  },
+  {
+    "revision": "653886481863f37579266b0e74ee2b68",
+    "url": "/StarSwap/index.html"
+  }
+];
